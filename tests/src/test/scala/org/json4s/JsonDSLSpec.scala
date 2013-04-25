2bac90b6caeac51cdbeb1a1be21517ff8ae232ea
xuwei-k <6b656e6a69@gmail.com>
2013-04-25 14:43:37 +0900

    add JsonDSLSpec

---
+++ tests/src/test/scala/org/json4s/JsonDSLSpec.scala
@@ -0,0 +1,31 @@
+package org.json4s
+
+import org.specs2.mutable.Specification
+import org.scalacheck._
+import org.scalacheck.Prop.forAllNoShrink
+import org.specs2.ScalaCheck
+import org.specs2.matcher.MatchResult
+
+object JsonDSLSpec extends Specification with JValueGen with ScalaCheck {
+
+  ("JSON DSL Specification") should {
+    "build Json" in {
+      import JsonDSL._
+      val buildProp = (intValue: Int, strValue: String) =>
+        (("intValue" -> intValue) ~ ("strValue" -> strValue)) must_== JObject("intValue" -> JInt(intValue), ("strValue", JString(strValue)))
+      prop(buildProp)
+    }
+
+    "customize value type" in {
+      object CustomJsonDSL extends JsonDSL with DoubleMode {
+        override def int2jvalue(x: Int) = super.int2jvalue(x)
+        implicit def customInt2jvalue(x: Int) = JString(x.toString)
+      }
+      import CustomJsonDSL._
+      val buildProp = (intValue: Int, strValue: String) =>
+        (("intValue" -> intValue) ~ ("strValue" -> strValue)) must_== JObject(("intValue", JString(intValue.toString)), ("strValue", JString(strValue)))
+      prop(buildProp)
+    }
+  }
+
+}
