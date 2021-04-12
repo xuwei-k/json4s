246a37122dc68c00bc2b1504680633fb7595d8b1
xuwei-k <6b656e6a69@gmail.com>
2021-04-12 09:33:00 +0900

    add case class Writer, Reader and JsonFormat methods

---
+++ ast/shared/src/test/scala/org/json4s/CaseClass22.scala
@@ -0,0 +1,29 @@
+package org.json4s
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
+
+case class CaseClass22(
+  a1: String,
+  a2: Boolean,
+  a3: Option[Int],
+  a4: List[String],
+  a5: Int,
+  a6: String,
+  a7: Boolean,
+  a8: Option[Int],
+  a9: List[String],
+  a10: Int,
+  a11: String,
+  a12: Boolean,
+  a13: Option[Int],
+  a14: List[String],
+  a15: Int,
+  a16: String,
+  a17: Boolean,
+  a18: Option[Int],
+  a19: List[String],
+  a20: Int,
+  a21: String,
+  a22: Boolean
+)
